e78bfa4488d782565ef9e1b131a5f341e099e343
NaruebeTh1 <b6419615@g.sut.ac.th>
2023-11-24 23:19:20 +0700

    update forntend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import type { MenuProps } from "antd";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import {
+  DeploymentUnitOutlined,
+  FileDoneOutlined,
+  PieChartOutlined,
+  ContainerOutlined,
+} from '@ant-design/icons';
+import { Avatar, Breadcrumb, Layout, Menu, Space, theme } from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ParcelInfoSummary from "./pages/parcelInfoSum/parcelInfoSummary";
+import MyParcelList from './pages/parcelList/myParcelList';
+import PinkUpParcelList from './pages/pinkUpParcel/pinkUpParcelList';
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[]
+): MenuItem {
+  return {key,icon,children,label,} as MenuItem;
 }
+
+const items: MenuItem[] = [
+
+  getItem('งานพัสดุโรงเรียน', 'sub1', <DeploymentUnitOutlined />, [
+    getItem(<Link to="/pages/parcelInfoSummary"> ภาพรวมงานพัสดุ </Link>, '1', <PieChartOutlined />),
+    getItem(<Link to="/pages/myParcelList"> รายการพัสดุ </Link>, '2', <FileDoneOutlined />),
+    getItem(<Link to="/pages/pinkUpParcelList"> รายการเบิกจ่ายพัสดุ </Link>, '3', <ContainerOutlined />),
+  ]),
+];
+
+
+const App: React.FC = () => {
+  
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  
+
+  return (
+    <Router>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+          
+          <div style={{ textAlign: 'center', marginTop: '40px', marginBottom: '30px'}}>
+            <Avatar src="https://xsgames.co/randomusers/avatar.php?g=pixel&key=1" style={{ cursor: 'pointer', transform: 'scale(2)' }} />
+          </div>
+
+          {!collapsed && (
+              <div style={{ textAlign: 'center', marginTop: '30px', color: 'white' }}>
+                My Name
+                {/* <a href="/login/user" style={{ textDecoration: "none" }}>
+                          <text style={{
+                              fontSize: '20px', marginLeft: '25px',
+                              fontWeight: 'bolder', color: 'white'
+                          }}>
+                              <span style={{ color: '#ff7518' }}>{FullName}</span>
+                          </text>
+                </a> */}
+              </div>
+          )}
+
+          <Menu theme="dark" style={{ marginTop: '20px'}} items={items} defaultSelectedKeys={["parcelList"]} mode="inline">
+          </Menu>
+          
+        </Sider>
+        <Layout>
+            <Header style={{ padding: 0, background: 'red' }}>
+              <div style={{textAlign: 'center', fontSize: '32px', fontWeight: 'bold'}}>
+                Topic of this page
+              </div>
+            </Header>
+
+            <Content style={{ margin: "0 16px" }}>
+
+              <Breadcrumb style={{ margin: "16px 0" }} />
+
+            <div style={{padding: 24,minHeight: "100%",background: colorBgContainer,}}>
+
+
+
+            <Routes>
+              <Route path="/pages/parcelInfoSummary" element={<ParcelInfoSummary />} />
+              <Route path="/pages/myParcelList" element={<MyParcelList />} />
+              <Route path="/pages/pinkUpParcelList" element={<PinkUpParcelList />} />
+            </Routes>
+
+
+            </div>
+
+          </Content>
+
+          <Footer style={{ textAlign: 'center', position: 'fixed', bottom: 0, width: '100%'}}>
+            <Space>
+              <div style={{fontWeight: 'bold'}}> 
+                Project SE ©2023 School Management System 
+              </div>
+            </Space>
+          </Footer>
+
+        </Layout>
+      </Layout>
+    </Router>
+  );
+};
 
 export default App;
