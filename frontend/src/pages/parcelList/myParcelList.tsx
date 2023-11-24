8b8b19d5ec6f0e42e8411c302e1f48ddacc64dfd
NaruebeTh1 <b6419615@g.sut.ac.th>
2023-11-24 23:10:40 +0700

    update

---
+++ frontend/src/pages/parcelList/myParcelList.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+
+export default function MyParcelList() {
+  return (
+    <> 
+    
+      TEST2 
+    
+    </>
+  );
+};
---
+++ frontend/src/pages/pinkUpParcel/pinkUpParcelList.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+import { Avatar, Card, Layout, Menu, Space} from 'antd';
+
+
+
+
+export default function PinkUpParcelList() {
+  return (
+    <> 
+      TEST1 
+    </>
+  );
+};
+
+
